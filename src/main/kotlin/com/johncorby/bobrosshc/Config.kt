cbba59af511bb14f7513ed996bdda9a3b537d39d
johncorby <williamforgames6@gmail.com>
2019-08-31 11:44:40 -0700

    use serialize instead of yaml (not tested)

--- src/main/kotlin/com/johncorby/bobrosshc/Config.kt
+++ src/main/kotlin/com/johncorby/bobrosshc/Config.kt
@@ -1,28 +1,28 @@
 package com.johncorby.bobrosshc
 
 import com.johncorby.bobrosshc.Main.Companion.PLUGIN
-import org.bukkit.configuration.file.YamlConfiguration
 import java.io.File
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
 
 object Config {
-    private const val CONFIG_NAME = "playerData.yml"
+    private const val NAME = "playerData.dat"
     private lateinit var file: File
-    private lateinit var config: YamlConfiguration
 
     private var data = mutableMapOf<String, Boolean>()
 
     fun load() {
 //        PLUGIN.dataFolder.mkdir()
-        file = File(PLUGIN.dataFolder, CONFIG_NAME)
-        config = YamlConfiguration.loadConfiguration(file)
+        file = File(PLUGIN.dataFolder, NAME)
 
-        data = config.getValues(false) as MutableMap<String, Boolean>
+        ObjectInputStream(file.inputStream()).use {
+            data = it.readObject() as MutableMap<String, Boolean>
+        }
     }
 
     fun save() {
-        for ((key, value) in data)
-            config[key] = value
-
-        config.save(file)
+        ObjectOutputStream(file.outputStream()).use {
+            it.writeObject(data)
+        }
     }
 }
