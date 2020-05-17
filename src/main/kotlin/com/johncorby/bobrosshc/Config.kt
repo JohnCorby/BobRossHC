fb14b9f3396ff0eab842657e2c44281b41c1c8ce
JohnCorby <williamforgames6@gmail.com>
2020-05-16 19:16:06 -0700

    type safety with config sorta (throws own error)

--- src/main/kotlin/com/johncorby/bobrosshc/Config.kt
+++ src/main/kotlin/com/johncorby/bobrosshc/Config.kt
@@ -3,14 +3,14 @@ package com.johncorby.bobrosshc
 import com.johncorby.coreapi.ConfigFile
 
 object Config : ConfigFile() {
-    val WORLD_PREFIX by Field("world-prefix", "HardCore_Season_")
-    val SEASON_DURATION by Field("season-duration", 14)
-    val NUM_SEASONS_TO_KEEP by Field("num-seasons-to-keep", 5)
-    val SEASON_CHECK_INTERVAL by Field("new-season-check-interval", 60)
+    val WORLD_PREFIX by field("world-prefix", "HardCore_Season_")
+    val SEASON_DURATION by field("season-duration", 14)
+    val NUM_SEASONS_TO_KEEP by field("num-seasons-to-keep", 5)
+    val SEASON_CHECK_INTERVAL by field("new-season-check-interval", 60)
 }
 
 object Data : ConfigFile("data.yml") {
-    var currentSeason by Field("current-season", 0)
-    var lastReset by Field<String?>("last-reset")
-    var deadPlayers by Field("dead-players", mutableListOf<String>())
+    var currentSeason by field("current-season", 0)
+    var lastReset by field<String?>("last-reset")
+    var deadPlayers by field("dead-players", mutableListOf<String>())
 }
